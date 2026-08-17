5eb1cf2be0c76437a33fdec20a4180bfa09f55e3
Egor Bulavintsev <egor.bulavintsev@digitalepoch.ru>
2026-08-18 01:28:03 +0300

    поправил миграцию

--- src/main/resources/db/migration/V0004__fix_sub_index.sql
+++ src/main/resources/db/migration/V0004__fix_sub_index.sql
@@ -1,5 +1,5 @@
-ALTER TABLE subscriptions DROP CONSTRAINT uq_subscription_user_channel;
+ALTER TABLE subscriptions DROP CONSTRAINT IF EXISTS uq_subscription_user_channel;
 
-CREATE UNIQUE INDEX uq_subscription_user_channel_active
+CREATE UNIQUE INDEX IF NOT EXISTS uq_subscription_user_channel_active
     ON subscriptions (user_id, channel_id)
     WHERE status = 'ACTIVE';
