4c67ecce81a3fad297216145a672931a38851875
Egor Bulavintsev <egor.bulavintsev@digitalepoch.ru>
2026-08-18 00:44:57 +0300

    добавил EntityGraph для инициализации lazy полей в @Scheduled методах

--- src/main/java/tg/bot/subscription/SubscriptionRepository.java
+++ src/main/java/tg/bot/subscription/SubscriptionRepository.java
@@ -2,6 +2,7 @@ package tg.bot.subscription;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tg.bot.user.User;
@@ -10,9 +11,11 @@ import tg.bot.user.User;
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
   List<Subscription> findByUserAndStatus(User user, SubscriptionStatus status);
 
+  @EntityGraph(attributePaths = "user")
   List<Subscription> findByExpireDateBeforeAndStatus(
       LocalDateTime dateTime, SubscriptionStatus status);
 
+  @EntityGraph(attributePaths = "user")
   List<Subscription> findByExpireDateBetweenAndReminderSentFalseAndStatus(
       LocalDateTime from, LocalDateTime to, SubscriptionStatus status);
 }
